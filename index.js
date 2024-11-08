42a26088e14f3392ed1a41218d89d1ddef4dce7e
haolem <lehao4032@gmail.com>
2024-11-08 16:41:34 +0700

    update 1

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+})
+
+app.get('/qs/:spec', (req, res) => {
+    const { page } = req.params;
+    const sendPath = path.join(__dirname, 'public', 'qs', `${page}.html`);
+
+    res.sendFile(sendPath, (err) => {
+        if (err) {
+            res.status(404).sendFile(path.join(__dirname, 'error_404.html'))
+        }
+    })
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'error_404.html'));
+  });
+app.listen(80, () => {
+    console.log('Server is running on port 80')
+})
